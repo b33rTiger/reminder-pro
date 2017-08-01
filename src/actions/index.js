1cd8a886606bf8c2e4ed380a1a1bdfa09bea5e5b
John <john@johnsheets.me>
2017-08-01 13:11:49 -0700

    clear all working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { ADD_REMINDER, DELETE_REMINDER, CLEAR_REMINDERS } from '../constants';
 
 export const addReminder = (text, dueDate) => {
   const action = {
@@ -13,6 +13,13 @@ export const deleteReminder = (id) => {
   const action = {
     type: DELETE_REMINDER,
     id
+  }
+  return action;
+}
+
+export const clearReminders = () => {
+  const action = {
+    type: CLEAR_REMINDERS
   }
   return action;
 }
